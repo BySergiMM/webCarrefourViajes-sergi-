93c3c772d555ef83bf03492bfaee22841f891348
BySergiMM <98814441+BySergiMM@users.noreply.github.com>
2023-03-08 19:32:43 +0100

    contacto
    falta arreglar que se inicia una pagina blanca despues

--- contacto/send_mail.php
+++ contacto/send_mail.php
@@ -1,18 +1,27 @@
 <?php
-if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $to = "vnalandasb@gmail.com"; // Dirección de correo del destinatario
-    $subject = "Mensaje de la web de, " . $name;
-    $body = "Has recibido un nuevo mensaje de " . $name . " (" . $email . "):\n\n" . $message;
-    $headers = "From: $email\r\n"; // Dirección de correo del remitente
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Tu mensaje ha sido enviado con éxito.";
+if (isset($_POST['submit'])) {
+    $name = trim($_POST['name'] ?? '');
+    $email = trim($_POST['email'] ?? '');
+    $message = trim($_POST['message'] ?? '');
+
+    $success_message = "";
+    $error_message = "";
+
+    if (empty($name) || empty($email) || empty($message)) {
+        $error_message = "Por favor, completa todos los campos del formulario.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error_message = "Por favor, introduce una dirección de correo electrónico válida.";
     } else {
-        echo "Ha habido un error al enviar tu mensaje. Por favor, inténtalo de nuevo más tarde.";
+        $to = "vnalandasb@gmail.com";
+        $subject = "Mensaje de la web de, " . $name;
+        $body = "Has recibido un nuevo mensaje de " . $name . " (" . $email . "):\n\n" . $message;
+        $headers = "From: $email\r\n";
+
+        if (mail($to, $subject, $body, $headers)) {
+            $success_message = "Tu mensaje ha sido enviado con éxito.";
+        } else {
+            $error_message = "Ha habido un error al enviar tu mensaje. Por favor, inténtalo de nuevo más tarde.";
+        }
     }
-} else {
-    echo "Por favor, completa todos los campos del formulario.";
 }
 ?>
