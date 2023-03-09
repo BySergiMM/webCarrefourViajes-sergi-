60fcd68f68f4b861a1563120fc8808c972bb42b7
BySergiMM <98814441+BySergiMM@users.noreply.github.com>
2023-03-09 17:52:49 +0100

    a

--- contacto/send_mail.php
+++ contacto/send_mail.php
@@ -1,27 +1,13 @@
 <?php
-if (isset($_POST['submit'])) {
-    $name = trim($_POST['name'] ?? '');
-    $email = trim($_POST['email'] ?? '');
-    $message = trim($_POST['message'] ?? '');
+if(isset($_POST['submit'])){
+    $to = "vnalandasb@gmail.com"; // Cambie la dirección de correo electrónico aquí
+    $from = $_POST['email']; // El remitente del correo electrónico es el correo electrónico del formulario.
+    $name = $_POST['name'];
+    $subject = "Formulario de contacto de Nalanda"; // El asunto del correo electrónico
+    $message = $name . " escribió: " . "\n\n" . $_POST['message']; // El mensaje del correo electrónico
 
-    $success_message = "";
-    $error_message = "";
+    $headers = "From:" . $from;
 
-    if (empty($name) || empty($email) || empty($message)) {
-        $error_message = "Por favor, completa todos los campos del formulario.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $error_message = "Por favor, introduce una dirección de correo electrónico válida.";
-    } else {
-        $to = "vnalandasb@gmail.com";
-        $subject = "Mensaje de la web de, " . $name;
-        $body = "Has recibido un nuevo mensaje de " . $name . " (" . $email . "):\n\n" . $message;
-        $headers = "From: $email\r\n";
-
-        if (mail($to, $subject, $body, $headers)) {
-            $success_message = "Tu mensaje ha sido enviado con éxito.";
-        } else {
-            $error_message = "Ha habido un error al enviar tu mensaje. Por favor, inténtalo de nuevo más tarde.";
-        }
-    }
+    mail($to,$subject,$message,$headers);
 }
 ?>
