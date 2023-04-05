1552c566b5ad732e83cb1299a1ad3a3e78769ca2
BySergiMM <98814441+BySergiMM@users.noreply.github.com>
2023-04-05 13:28:31 +0200

    05/04/2023
    todas las ofertas subidas
    quienes somos corregida
    enlaces corregidos
    fotos corregidas, falta 1 por corregir creo q en salidas especiales

--- contacto/send_mail.php
+++ contacto/send_mail.php
@@ -14,16 +14,7 @@
         $headers = "From: $name <$email>" . "\r\n";
         $headers .= "Reply-To: $email" . "\r\n";
         $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
-
-        // Mensaje a enviar
-        $email_body = "
-            <h2>Nuevo mensaje del formulario de contacto:</h2>
-            <p><strong>Nombre:</strong> $name</p>
-            <p><strong>Correo electrónico:</strong> $email</p>
-            <p><strong>Asunto:</strong> $subject</p>
-            <p><strong>Mensaje:</strong></p>
-            <p>$message</p>
-        ";
+        $email_body = "<p>$message</p>";
 
         // Envía el mensaje de correo electrónico
         if (mail($to, $subject, $email_body, $headers)) {
